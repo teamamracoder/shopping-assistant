d4198fa8e4d184a3146baa08a7738c8c6438bfda
diptibiswas <diptibca123@gmail.com>
2025-02-18 08:18:11 +0530

    modify urls

--- business_partner/urls/user_urls.py
+++ business_partner/urls/user_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ..views import UserListCreateAPIView, UserDetailAPIView
 
 urlpatterns = [
-    path('/users', UserListCreateAPIView.as_view(), name='user-list-create'),
-    path('/users/<int:pk>', UserDetailAPIView.as_view(), name='user-detail'),
+    path('users/', UserListCreateAPIView.as_view(), name='user-list-create'),
+    path('/users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
 ]
