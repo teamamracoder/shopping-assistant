23440b8887ed6294505001b8922f3d9410bca171
Sujit <das884536@gmail.com>
2025-02-23 13:26:47 -0800

    update user model

--- control_panel/models/user_model.py
+++ control_panel/models/user_model.py
@@ -51,29 +51,3 @@ class User(models.Model):
 
         def __str__(self):
             return f"ID: {self.id}, Created at: {self.created_at}, Active: {self.is_active}"
-
-
-
-
-
-
-
-
-
-
-
-
-# class UserModel(models.Model):
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField(unique=True)
-#     phone = models.CharField(max_length=15, null=True, blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     class Meta:
-#         db_table = 'users'
-#         verbose_name = 'User'
-#         verbose_name_plural = 'Users'
-
-#     def __str__(self):
-#         return self.name
