8fd446d58ed06b9ac8d0092f3567a7ecf9064842
Sujit <das884536@gmail.com>
2025-02-23 13:12:56 -0800

    users model added

---
+++ constants/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Gender(Enum):
+    MALE = 1
+    FEMALE = 2
+    OTHER = 3
+
+class Role(Enum):
+    END_USER = 1
+    SELLER = 2
+    SERVICE_PROVIDER = 3
+    ADMIN = 4
