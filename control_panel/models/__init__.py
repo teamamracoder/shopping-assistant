f6d11df4a611ebbbc54e89eae279ea3f13565797
TufanDas <dtufan38@gmail.com>
2025-02-23 19:34:47 +0530

    dghgdfh

--- control_panel/models/__init__.py
+++ control_panel/models/__init__.py
@@ -1,1 +1,4 @@
 from .user_model import *
+from .product_sub_category import *
+from .product_category import *
+from .product_model import *
---
+++ control_panel/models/product_category.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class ProductCategoryModel(models.Model):
+    id = models.AutoField(primary_key=True)  # Auto-incremented integer ID
+    name = models.CharField(max_length=255)  # Category name
+    description = models.TextField(blank=True, null=True)  # Optional description
+
+    is_active = models.BooleanField(db_default=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+    created_by = models.ForeignKey('UserModel', on_delete=models.CASCADE, blank=True, null=True, related_name='fk_create_product_categories_user_id')
+    updated_by = models.ForeignKey('UserModel', on_delete=models.CASCADE, blank=True, null=True, related_name='fk_update_product_categories_user_id')
+
+    def __str__(self):
+        return self.name
