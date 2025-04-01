8bb5d2ca9ef610410007a8b3d7b67ee08713a518
priya <priyamitra0606@gmail.com>
2025-04-02 00:43:38 +0530

    af

--- control_panel/forms/__init__.py
+++ control_panel/forms/__init__.py
@@ -1,1 +1,2 @@
 from .manage_store_form import *
+from .manage_store_category_form import *
--- control_panel/urls/manage_urls.py
+++ control_panel/urls/manage_urls.py
@@ -5,8 +5,17 @@ urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('dashboard/', ManageDashboardView.as_view(), name='manage_dashboard'),
     path('users/', ManageUserView.as_view(), name='manage_user_list'),
+
+# store urls by priya
     path('stores/', ManageStoreView.as_view(), name='manage_Store_list'),
     path('stores/create/', ManageCreateStore.as_view(), name='manage_create_store'),
     path('stores/update/<int:pk>/', ManageUpdateStoreView.as_view(), name='manage_update_store'),
+    path('toggle-store-status/<int:store_id>/', ToggleStoreStatus.as_view(), name='toggle_store_status'),
+
+#store category url
+    path('stores/category/', ManageStoreCategoryView.as_view(), name='manage_Store_category_list'),
+    path('store-category/create/', ManageCreateStoreCategoryView.as_view(), name='manage_create_store_category'),
+    path("store-category/delete/<int:category_id>/", ManageDeleteStoreCategoryView.as_view(), name="delete_store_category"),
+    path('toggle-store-category-status/<int:category_id>/', ToggleStoreCategoryStatus.as_view(), name='toggle_store_cetegory_status'),
   
 ]
