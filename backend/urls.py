ddf332210895833025ae035118e5107095fd87fb
Rahul-code-gif <ra1999sarkar@gmail.com>
2025-04-02 19:06:12 +0530

    hello

--- backend/urls.py
+++ backend/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 urlpatterns = [
     path("", lambda request: JsonResponse({"message": "Welcome"})),
     path("admin/", admin.site.urls),
-    path("api/v1/shopping-assistant", include("shopping_assistant.urls")),
-    path("api/v1/business-partner", include("business_partner.urls")),
-    path("control-panel", include("control_panel.urls")),
+    path("api/v1/shopping-assistant/", include("shopping_assistant.urls")),
+    path("api/v1/business-partner/", include("business_partner.urls")),
+    path("control-panel/", include("control_panel.urls")),
 ]
