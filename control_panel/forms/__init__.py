19deb584fba8f3b7d4b797e0b8a354c73c84c9b5
diptibiswas <diptibca123@gmail.com>
2025-04-03 11:57:38 +0530

    pull with sujit branch

--- control_panel/forms/__init__.py
+++ control_panel/forms/__init__.py
@@ -1,2 +1,3 @@
 from .manage_store_form import *
-from .manage_store_category_form import *from .manage_product_sub_category_form import *
+from .manage_store_category_form import *
+from .manage_product_sub_category_form import *
