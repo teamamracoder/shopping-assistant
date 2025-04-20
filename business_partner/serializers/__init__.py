75766081f58c11f9ff6eff956c6737009f952271
diptibiswas <diptibca123@gmail.com>
2025-04-20 19:53:41 +0530

    create API for service booking model

--- business_partner/serializers/__init__.py
+++ business_partner/serializers/__init__.py
@@ -1,7 +1,8 @@
 from .user_serializer import UserSerializer
 from .service_serializer import ServiceSerializer
 from .service_type_serializer import ServiceTypeModelSerializer
-from .service_booking_serializer import ServiceBookingModelSerializer
+from .service_booking_serializer import *
+# from .service_booking_serializer import ServiceBookingModelSerializer
 from .product_serializer import *
 from .product_category_serializer import *
 from .product_sub_category_serializer import *
--- services/service_booking_service.py
+++ services/service_booking_service.py
@@ -1,54 +1,24 @@
-from django.db import IntegrityError
-from django.forms import ValidationError
-from control_panel.models import ServiceBookingModel
+# services/service_booking_service.py
+from control_panel.models.service_booking_model import ServiceBookingModel
 
 class ServiceBookingModelService:
     def get_all_bookings(self):
-        """
-        Fetch all service bookings from the database.
-        :return: Queryset of all ServiceBookingModel instances.
-        """
         return ServiceBookingModel.objects.all()
 
     def get_booking_by_id(self, pk):
-        """
-        Fetch a service booking by its primary key (ID).
-        :param pk: Primary key (ID) of the service booking.
-        :return: ServiceBookingModel instance if found, None otherwise.
-        """
         try:
             return ServiceBookingModel.objects.get(pk=pk)
         except ServiceBookingModel.DoesNotExist:
             return None
 
-    def update_booking(self, booking, validated_data):
-        """
-        Update an existing service booking with new validated data.
-        :param booking: ServiceBookingModel instance to be updated.
-        :param validated_data: A dictionary of data to update the booking with.
-        :return: Updated ServiceBookingModel instance.
-        :raises ValidationError: If the updated data fails validation.
-        """
-        try:
-            booking.service = validated_data.get('service', booking.service)
-            booking.service_provider = validated_data.get('service_provider', booking.service_provider)
-            booking.user = validated_data.get('user', booking.user)
-            booking.booking_charge = validated_data.get('booking_charge', booking.booking_charge)
-            booking.booking_time = validated_data.get('booking_time', booking.booking_time)
-            
-            booking.save()
-            return booking
-        except IntegrityError:
-            raise ValidationError("Service booking update failed due to integrity issues.")
+    def create_booking(self, validated_data):
+        return ServiceBookingModel.objects.create(**validated_data)
 
-    def delete_booking(self, booking):
-        """
-        Delete a service booking.
-        :param booking: ServiceBookingModel instance to be deleted.
-        :return: None
-        :raises ValidationError: If deletion fails.
-        """
-        try:
-            booking.delete()
-        except Exception as e:
-            raise ValidationError(f"Error deleting service booking: {str(e)}")
+    def update_booking(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+    def delete_booking(self, instance):
+        instance.delete()
