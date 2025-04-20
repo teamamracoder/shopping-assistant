40d439832a5c0166c8d904960dd55fc6dc02e81a
Subham <modaksubham866@gmail.com>
2025-04-20 13:36:12 +0530

    update url

--- business_partner/urls/product_category_urls.py
+++ business_partner/urls/product_category_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ..views.product_category_view import *
 
 urlpatterns = [
-    path('productcategories', ProductCategoryListCreateAPIView.as_view(), name='product-category-list-create'),
-    path('productcategories/<int:pk>',ProductCategoryDetailAPIView.as_view(), name='product-category-detail'),
+    path('productcategories/', ProductCategoryListCreateAPIView.as_view(), name='product-category-list-create'),
+    path('productcategories/<int:pk>/',ProductCategoryDetailAPIView.as_view(), name='product-category-detail'),
 ]
