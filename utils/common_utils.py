ff107716758ac2ced49d5ab914c5a5f96be9f9a4
SubhasisBarman <subhasisbarman9288@gmail.com>
2025-09-11 07:14:21 +0530

    get_user_id issue solved

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 
+from control_panel.models.user_model import UserModel
+
 
 def print_log(*args):
     if settings.DEBUG:
         print(args)
 
 def get_user_id(request):
-        return request.session.get('auth', {}).get('user', {}).get('id')
+        user_id=request.session.get('auth', {}).get('user', {}).get('id')
+        return UserModel.objects.get(pk=user_id)
